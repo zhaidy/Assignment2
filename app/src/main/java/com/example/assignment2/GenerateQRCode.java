daed83676ec78e6b39f6b2a11348ff4558982283
zhaidy <dongyuzhai@gmail.com>
2021-06-28 23:48:10 +0800

    Able to generate QR code

---
+++ app/src/main/java/com/example/assignment2/GenerateQRCode.java
@@ -0,0 +1,87 @@
+package com.example.assignment2;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+
+
+public class GenerateQRCode extends AppCompatActivity {
+    public final static int QRcodeWidth = 350 ;
+    EditText editText;
+    ImageView imageView;
+    String EditTextValue ;
+    Bitmap bitmap ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.generate_qr_code);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        editText = (EditText) findViewById(R.id.editText);
+    }
+
+    public void onClick(View view) {
+        if(!editText.getText().toString().isEmpty()){
+            EditTextValue = editText.getText().toString();
+
+            try {
+                bitmap = TextToImageEncode(EditTextValue);
+
+                imageView.setImageBitmap(bitmap);
+
+            } catch (WriterException e) {
+                e.printStackTrace();
+            }
+        }
+        else{
+            editText.requestFocus();
+            Toast.makeText(this, "Please Enter Your Scanned Test" , Toast.LENGTH_LONG).show();
+        }
+    }
+    Bitmap TextToImageEncode(String Value) throws WriterException {
+        BitMatrix bitMatrix;
+        try {
+            bitMatrix = new MultiFormatWriter().encode(
+                    Value,
+                    BarcodeFormat.DATA_MATRIX.QR_CODE,
+                    QRcodeWidth, QRcodeWidth, null
+            );
+
+        } catch (IllegalArgumentException Illegalargumentexception) {
+
+            return null;
+        }
+        int bitMatrixWidth = bitMatrix.getWidth();
+
+        int bitMatrixHeight = bitMatrix.getHeight();
+
+        int[] pixels = new int[bitMatrixWidth * bitMatrixHeight];
+
+        for (int y = 0; y < bitMatrixHeight; y++) {
+            int offset = y * bitMatrixWidth;
+
+            for (int x = 0; x < bitMatrixWidth; x++) {
+
+                if (bitMatrix.get(x, y))
+                    pixels[offset + x] = ContextCompat.getColor(this, R.color.black);
+                else pixels[offset + x] = ContextCompat.getColor(this, R.color.white);
+            }
+        }
+        Bitmap bitmap = Bitmap.createBitmap(bitMatrixWidth, bitMatrixHeight, Bitmap.Config.ARGB_4444);
+
+        bitmap.setPixels(pixels, 0, 350, 0, 0, bitMatrixWidth, bitMatrixHeight);
+        return bitmap;
+    }
+}
--- app/src/main/java/com/example/assignment2/MainActivity.java
+++ app/src/main/java/com/example/assignment2/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.assignment2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //testing 1 2 3
+    }
+
+    public void btnGenerate(View view){
+        Intent i = new Intent(this, GenerateQRCode.class);
+        startActivity(i);
     }
 }
