a52b371b91302bf1b4df985728d419bc418d41a2
Samuel-HT <samuel199331@gmail.com>
2021-07-01 23:30:08 +0800

    historyQr

---
+++ app/src/main/java/com/example/assignment2/HistoryQR.java
@@ -0,0 +1,14 @@
+package com.example.assignment2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HistoryQR extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history_qr);
+    }
+}
