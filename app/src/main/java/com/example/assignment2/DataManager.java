5f5d0ac9dcc6e7305cab1c595ebce7b693c94805
zhaidy <dongyuzhai@gmail.com>
2021-07-20 19:32:26 +0800

    Added some columns in DB, need to uninstall the app and run again.

--- app/src/main/java/com/example/assignment2/DataManager.java
+++ app/src/main/java/com/example/assignment2/DataManager.java
@@ -5,9 +5,14 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.graphics.Bitmap;
 import android.util.Log;
 
-public class DataManager {
+import java.io.ByteArrayOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DataManager{
     // This is the actual database
     private SQLiteDatabase db;
 
@@ -17,7 +22,9 @@ public class DataManager {
     */
     public static final String TABLE_ROW_ID = "_id";
     public static final String TABLE_ROW_SCANNEDQRCODE = "code";
-    //public static final String TABLE_ROW_TYPE = "type";
+    public static final String TABLE_ROW_TYPE = "type";
+    public static final String TABLE_ROW_IMAGE = "image";
+    public static final String TABLE_ROW_DATE = "date";
 
     /*
         Next we have a private static final strings for
@@ -44,32 +51,62 @@ public class DataManager {
         @Override
         public void onCreate(SQLiteDatabase db) {
             // Create a table for photos and all their details
-            String newTableQueryString = "create table " + QR_Table + " ("
-                    + TABLE_ROW_ID
-                    + " integer primary key autoincrement not null," + TABLE_ROW_SCANNEDQRCODE
-                    /*+ " text not null,"
-                    + TABLE_ROW_TYPE*/
-                    + " text not null);";
-            db.execSQL(newTableQueryString);
+            db.execSQL(
+                    "create table " +  QR_Table + "(" +
+                            TABLE_ROW_ID + " integer primary key autoincrement not null, " +
+                            TABLE_ROW_SCANNEDQRCODE   + " text, " +
+                            TABLE_ROW_TYPE  + " text, " +
+                            TABLE_ROW_IMAGE  + " text, " +
+                            TABLE_ROW_DATE + " date " + ")"
+            );
         }
 
         // This method only runs when we increment DB_VERSION
         @Override
-        public void onUpgrade(SQLiteDatabase db,
-                              int oldVersion, int newVersion) {
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS " + QR_Table + "");
+            onCreate(db);
         }
     }
 
-    public void insertHistory(String code){
-        String query = "INSERT INTO " + QR_Table + " (" + TABLE_ROW_SCANNEDQRCODE + ") " + "VALUES (" + "'" + code + "'" + ");";
-        Log.i("insert() + ", query);
-        db.execSQL(query);
+//    public void insertHistory(String type, String code, Bitmap img){
+//        String query = "INSERT INTO " + QR_Table + " (" + TABLE_ROW_SCANNEDQRCODE + ") " + "VALUES (" + "'" + code + "'" + ");";
+//        Log.i("insert() + ", query);
+//        db.execSQL(query);
+//    }
+
+    public boolean insertHistory(String type, String code, Bitmap img)
+    {
+        // Prepare the row to insert
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(TABLE_ROW_TYPE, type);
+        if(code != null && code != "") {
+            contentValues.put(TABLE_ROW_SCANNEDQRCODE, code);
+        }
+        if(img != null) {
+            byte[] data = getBitmapAsByteArray(img);
+            contentValues.put(TABLE_ROW_IMAGE, data);
+        }
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date = new Date();
+        contentValues.put(TABLE_ROW_DATE, dateFormat.format(date));
+
+        // Insert the row
+        db.insert(QR_Table, null, contentValues);
+        return true;
     }
 
     public Cursor getHistory(){
-        String query = "SELECT " + "(" + TABLE_ROW_SCANNEDQRCODE + ") " + "FROM " + QR_Table + ";";
+        String query = "SELECT  * FROM " + QR_Table + ";";
         Cursor c = db.rawQuery(query, null);
         return c;
     }
 
+    public static byte[] getBitmapAsByteArray(Bitmap bitmap) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 0, outputStream);
+        return outputStream.toByteArray();
+    }
 }
